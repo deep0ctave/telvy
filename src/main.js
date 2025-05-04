6a84e24a00cc23120e0b6e9ef97ef93b2518c740
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-05-05 00:09:31 +0530

    Create a template Hero Page

--- src/main.js
+++ src/main.js
@@ -3,14 +3,18 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 
 document.querySelector('#app').innerHTML = `
-<div data-theme="cyberpunk">
-  <div class="hero bg-base-200 min-h-screen">
+<div data-theme="silk" min-h-screen>
+
+<div class="navbar bg-base-100 shadow-sm">
+  <a class="btn btn-ghost text-xl">telvy</a>
+</div>
+
+<div class="hero bg-base-200" style="min-height: 90vh;">
   <div class="hero-content flex-col lg:flex-row-reverse">
     <div class="text-center lg:text-left">
       <h1 class="text-5xl font-bold">Login now!</h1>
-      <p class="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      <p class="py-2">
+        Just a step away from your next <strong>BIG</strong> question.
       </p>
     </div>
     <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
